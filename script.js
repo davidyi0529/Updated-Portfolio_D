f8b9e88d8d20f1c04f05fe2f6c9b6c790498e620
David Yi <david.yi.0529@gmail.com>
2020-10-07 13:07:45 -0700

    fixed index and style

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-(function($) {
-    "use strict"; // Start of use strict
-  
+
     // Closes the sidebar menu
     $(".menu-toggle").click(function(e) {
       e.preventDefault();
@@ -8,21 +6,7 @@
       $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
       $(this).toggleClass("active");
     });
-  
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000, "easeInOutExpo");
-          return false;
-        }
-      }
-    });
-  
+    
     // Closes responsive menu when a scroll trigger link is clicked
     $('#sidebar-wrapper .js-scroll-trigger').click(function() {
       $("#sidebar-wrapper").removeClass("active");
@@ -39,8 +23,6 @@
         $('.scroll-to-top').fadeOut();
       }
     });
-  
-  })(jQuery); // End of use strict
 
   var textWrapper = document.querySelector('.ml2');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
